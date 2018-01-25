8612a449cd25dd3e7bb101004de15143e5205215
kevin <kevinochieng548@gmail.com>
2018-01-25 14:25:04 +0300

    loop array backwards

--- vacation.js
+++ vacation.js
@@ -5,5 +5,9 @@ console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
 
 for (var i = 0; i < vacationSpots.length; i++) {
-    console.log('I would love to visit: ' + vacationSpots[i])
+    console.log('I would love to visit: ' + vacationSpots[i]);
+}
+
+for (var i = vacationSpots.length - 1; i >= 0; i--) {
+    console.log('I would love to visit: ' + vacationSpots[i]);
 }
