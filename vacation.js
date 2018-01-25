424d8f2a7af23cbacafa5a4e84ed1d35e01cb2b8
kevin <kevinochieng548@gmail.com>
2018-01-25 14:20:22 +0300

    loop through the array elements

--- vacation.js
+++ vacation.js
@@ -3,3 +3,7 @@ var vacationSpots = ['Rio', 'Dubai','Paris'];
 console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
+
+for (var i = 0; i < vacationSpots.length; i++) {
+    console.log('I would love to visit: ' + vacationSpots[i])
+}
