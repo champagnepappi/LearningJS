d58cd5a3fb4c64d3f187a7fa8949324fc24d3be9
kevin <kevinochieng548@gmail.com>
2018-01-25 14:17:22 +0300

    retrieve array elements

--- vacation.js
+++ vacation.js
@@ -1,1 +1,5 @@
-var vacationSpots = ['Rio', 'Dubai','Paris']
+var vacationSpots = ['Rio', 'Dubai','Paris'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
