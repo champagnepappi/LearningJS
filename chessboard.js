99db12893952dedf18918b3e119f0ab3f1aa7450
kevin <kevinochieng548@gmail.com>
2018-01-29 11:44:58 +0300

    check if i is even then add space

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,14 @@
 var size = 8;
 var block = '#';
 var space = ' ';
+
+for (var i = 1; i<= size; i++) {
+    var line ='';
+    for (var y = 1; y<= size; y++) {
+        if (i%2) {
+            if (y%2){
+                 line = line + space;
+            }
+        }
+    }
+}
