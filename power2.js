f0215b4a2d22478207f1bf2da4eba9ec3c288e1f
kevin <kevinochieng548@gmail.com>
2018-01-29 12:18:17 +0300

    define the power function

--- power2.js
+++ power2.js
@@ -3,3 +3,12 @@ var makeNoise = function() {
 };
 
 makeNoise();
+
+var power = function(base, exponent) {
+    var result = 1;
+    for (var count= 0; count < exponent; count++){
+        result = *base;
+        return result;
+    }
+};
+
