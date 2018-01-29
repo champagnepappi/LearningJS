72197ef73747d4fd92cc06c6d034a236c41965e4
kevin <kevinochieng548@gmail.com>
2018-01-29 12:20:55 +0300

    modify the power function

--- power2.js
+++ power2.js
@@ -7,8 +7,9 @@ makeNoise();
 var power = function(base, exponent) {
     var result = 1;
     for (var count= 0; count < exponent; count++){
-        result = *base;
-        return result;
+        result *= base;
     }
+    return result;
 };
 
+console.log(power(2,10));
