b5d4e1f4e05d9abaa348f97f0708732ef5bddc1c
kevin <kevinochieng548@gmail.com>
2018-01-29 11:47:54 +0300

    output the line

--- chessboard.js
+++ chessboard.js
@@ -8,7 +8,16 @@ for (var i = 1; i<= size; i++) {
         if (i%2) {
             if (y%2){
                  line = line + space;
+            } else {
+                 line = line + block;
+            }
+        } else {
+            if(y%2) {
+                line = line + block;
+            } else {
+                 line = line + space;
             }
         }
     }
+    console.log(line);
 }
